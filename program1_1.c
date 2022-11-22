bdbda6d30b6f999d64be8d68ad3f28ef233605a0
Tommy <47206288+tommysr@users.noreply.github.com>
2022-11-22 19:09:33 +0000

    /n in p1

--- program1_1.c
+++ program1_1.c
@@ -4,7 +4,7 @@
 
 void print_process_info()
 {
-  printf("PID: %d\n", getpid());
+  printf("\nPID: %d\n", getpid());
   printf("PPID: %d\n", getppid());
   printf("UID: %d\n", getuid());
   printf("GID: %d\n", getgid());
